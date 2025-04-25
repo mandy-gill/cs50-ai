2ca3c316a27d2ddcab87fa715de7f05d7b3d5f2f
mandy-gill <134782587+mandy-gill@users.noreply.github.com>
2025-04-25 17:15:01 -0500

    Implement some functions in tictatoe.py

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -8,6 +8,8 @@ X = "X"
 O = "O"
 EMPTY = None
 
+num_moves_played = 0
+
 
 def initial_state():
     """
@@ -22,28 +24,101 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    
+    # Get number of Xs on the board
+    num_Xs = 0
+    for i in len(board):
+        for j in len(board[i]):
+            if board[i][j] == X:
+                num_Xs += 1
+
+    if num_Xs % 2 == 0:
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    actions_set = set()
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == EMPTY:
+                actions_set.add((i,j))
+    return actions_set
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    # Check if action is valid 
+    if board[action[0]][action[1]] != EMPTY:
+        raise Exception("Invalid action.")
+
+    # Make a deep copy of the board
+    board_copy = []
+    for i in range(len(board)):
+        row = []
+        for j in range(len(board[i])):
+            row.append(board[i][j])
+        board_copy.append(row)
+
+    # Get the player whose turn it is
+    player_turn = player(board)
+
+    # Add the player's mark on the deep copy of the board
+    board_copy[action[0]][action[1]] = player_turn
+
+    return board_copy
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+
+    win_player = None
+    
+    # Check horizontally
+    all_equal = True
+    for i in range(len(board)):
+        first = board[i][0]
+        for j in range(len(board[i])):
+            if first != board[i][j]:
+                all_equal = False
+        if all_equal:
+            win_player = first
+            return win_player
+
+    # Check vertically
+    all_equal = True
+    for i in range(len(board)):
+        first = board[i][0]
+        for j in range(len(board[i])):
+            if first != board[j][i]:
+                all_equal = False
+        if all_equal:
+            win_player = first
+            return win_player
+
+    # Check main diagonal
+    all_equal = True
+    first = board[i][0]
+    for i in range(len(board)):
+        if (first) != board[i][i]:
+            all_equal = False
+    if all_equal:
+        win_player = first
+        return win_player
+    
+    # Check other diagonal
+            
+
+    return win_player
 
 
 def terminal(board):
